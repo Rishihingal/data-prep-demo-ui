990e3a22b0e4807ee0532cde0daf54c952125a26
RishiHingal <rishihingal@gmail.com>
2024-07-21 22:59:58 +0530

    added api for Eda stats

--- components/Eda.tsx
+++ components/Eda.tsx
@@ -1,36 +1,10 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Tabs, Tab, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Box, Card } from '@mui/material';
+import axios from 'axios';
 
-const summaryStatistics = {
-    "mean": {
-        "Age": 41.575,
-        "Motivation_Score": 5.551,
-        "Previous_Volunteer_Experience": 0.49,
-        "Time_Availability": 10.834,
-        "Volunteer_Suitability_Score": 0.496
-    },
-    "median": {
-        "Age": 42.0,
-        "Motivation_Score": 6.0,
-        "Previous_Volunteer_Experience": 0.0,
-        "Time_Availability": 11.0,
-        "Volunteer_Suitability_Score": 0.0
-    },
-    "mode": {
-        "Age": 43,
-        "Motivation_Score": 6,
-        "Previous_Volunteer_Experience": 0,
-        "Time_Availability": 13,
-        "Volunteer_Suitability_Score": 0
-    },
-    "standard_deviation": {
-        "Age": 13.758792643251805,
-        "Motivation_Score": 2.904375836561102,
-        "Previous_Volunteer_Experience": 0.4998999899979995,
-        "Time_Availability": 5.6528261958068375,
-        "Volunteer_Suitability_Score": 0.4999839997439918
-    }
-};
+interface SummaryStatistics {
+    [key: string]: Record<string, number>;
+}
 
 interface DataTableProps {
     data: Record<string, number>;
@@ -42,16 +16,16 @@ const DataTable: React.FC<DataTableProps> = ({ data }) => {
 
     return (
         <TableContainer component={Paper} className="max-h-64 mt-5 bg-gray-900 rounded-lg overflow-auto">
-            <Table size="medium" sx={{bgcolor:'Background.default'}}>
+            <Table size="medium" sx={{ bgcolor: 'Background.default' }}>
                 <TableHead>
-                    <TableRow sx={{bgcolor:'Background.default'}}>
+                    <TableRow sx={{ bgcolor: 'Background.default' }}>
                         {columns.map((column, index) => (
                             <TableCell key={index}>{column}</TableCell>
                         ))}
                     </TableRow>
                 </TableHead>
-                <TableBody sx={{bgcolor:'Background.default'}}>
-                    <TableRow sx={{bgcolor:'Background.default'}}>
+                <TableBody sx={{ bgcolor: 'Background.default' }}>
+                    <TableRow sx={{ bgcolor: 'Background.default' }}>
                         {values.map((value, index) => (
                             <TableCell key={index}>{value}</TableCell>
                         ))}
@@ -64,7 +38,26 @@ const DataTable: React.FC<DataTableProps> = ({ data }) => {
 
 const Eda: React.FC = () => {
     const [value, setValue] = useState(0);
-    const [tableData, setTableData] = useState<Record<string, number>>(summaryStatistics.mean);
+    const [tableData, setTableData] = useState<Record<string, number>>({});
+    const [summaryStatistics, setSummaryStatistics] = useState<SummaryStatistics>({});
+
+    useEffect(() => {
+        getData();
+    }, []);
+
+    const getData = async () => {
+        try {
+            const response = await axios.get('http://3.108.249.79:5000/stats', {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+            setSummaryStatistics(response.data['summary_statistics']);
+            setTableData(response.data['summary_statistics'].mean);
+        } catch (error) {
+            console.log('Error getting data:', error);
+        }
+    };
 
     const handleTabChange = (event: React.SyntheticEvent, newValue: number) => {
         setValue(newValue);
